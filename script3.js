bd0aadd17caf3e4b2be845ee18d4a63243560560
Sanobhi <sanobhi844@gmail.com>
2026-04-16 06:53:53 +0500

    Add files via upload

--- script3.js
+++ script3.js
@@ -20,7 +20,7 @@ function secoundsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
+    let a = await fetch(`/${folder}/`)
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response
@@ -91,7 +91,7 @@ playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:5500/songs/`)
+    let a = await fetch(/songs/)
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response
@@ -109,8 +109,9 @@ async function displayAlbums() {
         if (e.href.includes("/songs/") && !e.href.endsWith(".mp3")) {
             let folder = e.href.split("/").pop()
             // console.log(folder)
+            
             // Get the meta-data of the folder
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
+            let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json()
             cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
 
